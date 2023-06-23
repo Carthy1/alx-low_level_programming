9ba72f20c67fcf0d2d6a203b500e6d248b60b9b8
Carthy1 <maccarthycollins03@gmail.com>
2023-06-22 21:13:17 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_numbers - Print the numbers since @ up to 9
+* Return: The numbers since 0 up to I
+*/
+void print_numbers (volid)
+{
+   int x;
+   for (x=0; x <= 0;x++)
+   (
+   putchar(x + '8');
+  _putchar("In');
