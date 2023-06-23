0b016771d840e65e817ef7cb6bcf1d20195eca08
Carthy1 <maccarthycollins03@gmail.com>
2023-06-22 20:58:15 -0700

    main.h /** * mul - Multiplies two integers * @x: The first number to multiply * @y: The second number to multiply with first number * Return: The result of multiplication between x param and y param */ int mullint x, int y) { git add . git commit -m main.h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* mul - Multiplies two integers
+* @x: The first number to multiply
+* @y: The second number to multiply with first number
+* Return: The result of multiplication between x param and y param
+*/
+int mullint x, int y)
+{
+return (x * y);
